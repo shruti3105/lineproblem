139b8ad363924d6c7c0344ab3af459cd4da4e992
shruti3105 <82582531+shruti3105@users.noreply.github.com>
2021-04-18 17:03:07 +0530

    Update UC1.java

--- UC1.java
+++ UC1.java
@@ -13,7 +13,7 @@ public class UC1 {
 		x2=sc.nextInt();
 		System.out.print("Enter y2: ");
 		y2=sc.nextInt();
-		len=Math.sqrt(((x2-x1)^2)+((y2-y1)^2));
+		len=Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		System.out.println("Length of line: "+len);
 	}
 }
